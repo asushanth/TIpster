6e16f30dddb807b305dded125f0f78156551a9c1
Adarsh Sushanth <absushanth@gmail.com>
2020-11-11 12:01:15 -0500

    Initial Commit

---
+++ TIpster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TIpster
+//
+//  Created by Adarsh Sushanth on 11/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
